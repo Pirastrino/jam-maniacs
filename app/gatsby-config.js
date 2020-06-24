74d91f08a91bafc7a1205cb0ebaa56a94285d59a
Martin Began <martin.began@kiwi.com>
2020-06-25 01:53:09 +0200

    feat(setup): first init

---
+++ app/gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Maniacs Brno',
+    description: 'ClashOfClans Clan Page | Maniacs Brno',
+    author: {
+      name: 'Martin Began',
+    },
+  },
+  plugins: [
+    // 'gatsby-plugin-typescript',
+    // 'gatsby-plugin-react-helmet',
+    // 'gatsby-plugin-material-ui',
+    // {
+    //   resolve: 'gatsby-plugin-material-ui',
+    //   options: {
+    //     stylesProvider: {
+    //       injectFirst: true,
+    //     },
+    //   },
+    // },
+  ],
+};
