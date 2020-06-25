4685287c7f60b073932e771a8674f65fb15dd2db
Martin Began <martin.began@kiwi.com>
2020-06-25 02:52:15 +0200

    feat(test): test build from a PR

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>Welcome clashers!</h1>;
+  return (
+    <>
+      <h1>Welcome clashers!</h1>
+      <p>Maniacs Brno!</p>
+    </>
+  );
 };
 
 export default App;
